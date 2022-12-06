5d527b03b465314f92ac17b94fd9bc963e3399c6
tomoron <tomoron326@outlook.jp>
2022-12-06 20:15:06 +0900

    thread

--- main.rb
+++ main.rb
@@ -1,31 +1,19 @@
 require 'socket'
-require 'date'
+require './server_thread'
 
 server = TCPServer.open(3030)
 
 begin
-  socket = server.accept
-
-  while (line = socket.gets) != "\r\n"
-    path = line.split(' ')[1] if line&.start_with?('GET')
-  end
-
-  utc_date_string = DateTime.now.new_offset.strftime('%a, %d, %b, %Y, %H:%M:%S %Z GMT')
-
-  socket.write("HTTP/1.1 OK\n")
-  socket.write("Date: #{utc_date_string}\n")
-  socket.write("Server: RubySimpleServer\n")
-  socket.write("Connection: close\n")
-  socket.write("Content-type: text/html\n")
-
-  socket.write("\r\n")
-
-  File.foreach("src/#{path}") do |line|
-    socket.write(line)
+  loop do
+    puts 'クライアントからの接続を待ちます'
+    Thread.start(server.accept) do |socket|
+      puts 'クライアント接続'
+      server_thread = ServerThread.new(socket)
+      server_thread.run
+    end
   end
 rescue StandardError => e
   puts e
 ensure
-  socket.close
   server.close
 end
---
+++ server_thread.rb
@@ -0,0 +1,31 @@
+require 'socket'
+require 'date'
+
+class ServerThread
+  def initialize(socket)
+    @socket = socket
+  end
+
+  def run
+    while (line = @socket.gets) != "\r\n"
+      path = line.split(' ')[1] if line&.start_with?('GET')
+    end
+
+    utc_date_string = DateTime.now.new_offset.strftime('%a, %d, %b, %Y, %H:%M:%S %Z GMT')
+
+    @socket.write("HTTP/1.1 OK\n")
+    @socket.write("Date: #{utc_date_string}\n")
+    @socket.write("Server: RubySimpleServer\n")
+    @socket.write("Connection: close\n")
+    @socket.write("Content-type: text/html\n")
+    @socket.write("\r\n")
+
+    File.foreach("src/#{path}") do |line|
+      @socket.write(line)
+    end
+  rescue StandardError => e
+    puts e
+  ensure
+    @socket.close
+  end
+end
