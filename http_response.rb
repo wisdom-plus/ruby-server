a3b3a79e1a42d1a31eb758f11d69e4a40c616abd
tomoron <tomoron326@outlook.jp>
2022-12-07 20:27:45 +0900

    image

--- http_response.rb
+++ http_response.rb
@@ -1,23 +1,24 @@
 require './util'
 
 module HttpResponse
-  PATH_TO_NOT_FOUND = 'src/404.html'
-  SERVER_NAME = 'RubySimpleServer'
+  PATH_TO_NOT_FOUND = 'src/404.html'.freeze
+  SERVER_NAME = 'RubySimpleServer'.freeze
 
   class << self
     def send_ok(socket, path)
+      ext = Util.ext(path)&.to_sym
       socket.write("HTTP/1.1 200 OK\n")
-      socket.write("Date: #{Util.get_utc_date_string}\n")
+      socket.write("Date: #{Util.utc_date_string}\n")
       socket.write("Server: #{SERVER_NAME}\n")
       socket.write("Connection: close\n")
-      socket.write("Content-type: text/html\n")
+      socket.write("Content-type: #{Util.content_type(ext)}\n")
       socket.write("\r\n")
       write_body(socket, path)
     end
 
     def send_not_found(socket)
       socket.write("HTTP/1.1 404 Not Found\n")
-      socket.write("Date: #{Util.get_utc_date_string}\n")
+      socket.write("Date: #{Util.utc_date_string}\n")
       socket.write("Server: #{SERVER_NAME}\n")
       socket.write("Connection: close\n")
       socket.write("Content-type: text/html\n")
--- util.rb
+++ util.rb
@@ -1,9 +1,30 @@
 require 'date'
 
 module Util
+  CONTENT_TYPES = {
+    html: 'text/html',
+    htm: 'text.html',
+    txt: 'text/plain',
+    css: 'text/css',
+    csv: 'text/csv',
+    json: 'application/json',
+    png: 'image/png',
+    jpg: 'image/jpeg',
+    jpeg: 'image/jpeg',
+    gif: 'image/gif'
+  }.freeze
+
   class << self
-    def get_utc_date_string
-      utc_date_string = DateTime.now.new_offset.strftime('%a, %d, %b, %Y, %H:%M:%S %Z GMT')
+    def utc_date_string
+      DateTime.now.new_offset.strftime('%a, %d, %b, %Y, %H:%M:%S %Z GMT')
+    end
+
+    def ext(path)
+      File.extname(path).gsub('.', '')
+    end
+
+    def content_type(ext)
+      CONTENT_TYPES[ext] || 'application/octet-stream'
     end
   end
 end
